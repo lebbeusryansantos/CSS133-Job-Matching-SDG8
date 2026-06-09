f3d0dcdf304ba92b64c698365e8ceae00471e673
Nikolai Lagarde <nplagarde@mymail.mapua.edu.ph>
2026-06-09 15:16:18 +0800

    Optimize Gale-Shapley matching loop to O(n^2) using dict lookups and deque

--- gale_shapley.py
+++ gale_shapley.py
@@ -1,55 +1,74 @@
 import pandas as pd
 
 def generate_preference_dicts(applicants_df, jobs_df):
+    """Generates preference lists and O(1) ranking dictionaries."""
+
+    # Pre-extract data into dictionaries to avoid slow Pandas dataframe copying in loops 
+    app_skills = applicants_df.set_index('Applicant_ID')['Skill_Level'].to_dict()
+    app_exp = applicants_df.set_index('Applicant_ID')['Years_Experience'].to_dict()
+    app_salary = applicants_df.set_index('Applicant_ID')['Expected_Salary'].to_dict()
+
+    job_req_skills = jobs_df.set_index('Job_ID')['Required_Skill_Level'].to_dict()
+    job_req_exp = jobs_df.set_index('Job_ID')['Required_Experience'].to_dict()
+    job_offer = jobs_df.set_index('Job_ID')['Salary_Offer'].to_dict()
+    
     employer_prefs = {}
-    for _, job in jobs_df.iterrows():
-        scores = applicants_df.copy()
-        scores['score'] = (scores['Skill_Level'] - job['Required_Skill_Level']) + \
-                          (scores['Years_Experience'] - job['Required_Experience'])
-        
-        sorted_applicants = scores.sort_values(by='score', ascending=False)['Applicant_ID'].tolist()
-        employer_prefs[job['Job_ID']] = sorted_applicants
+    employer_ranks = {} # O(1) lookup dictionary [cite: 33]
 
+    # Generate Employer Preferences [cite: 34]
+    for job_id in job_req_skills.keys():
+        scores = []
+        for app_id in app_skills.keys():
+            score = (app_skills[app_id] - job_req_skills[job_id]) + \
+                    (app_exp[app_id] - job_req_exp[job_id])
+            scores.append((app_id, score))
+
+        # Sort descending by score [cite: 41]
+        scores.sort(key=lambda x: x[1], reverse=True)
+        sorted_applicants = [x[0] for x in scores]
+
+        employer_prefs[job_id] = sorted_applicants
+        # Map applicant ID to their rank index for instant O(1) lookup later [cite: 45, 46]
+        employer_ranks[job_id] = {app: rank for rank, app in enumerate(sorted_applicants)}
+    
     applicant_prefs = {}
-    for _, app in applicants_df.iterrows():
-        scores = jobs_df.copy()
-        scores['score'] = scores['Salary_Offer'] - app['Expected_Salary']
-        
-        sorted_jobs = scores.sort_values(by='score', ascending=False)['Job_ID'].tolist()
-        applicant_prefs[app['Applicant_ID']] = sorted_jobs
-        
-    return applicant_prefs, employer_prefs
 
-def gale_shapley(applicants_df, jobs_df):
-    applicant_prefs, employer_prefs = generate_preference_dicts(applicants_df, jobs_df)
-    
+    # Generate Applicant Preferences
+    for app_id in app_salary.keys():
+        scores = []
+        for job_id in job_offer.keys():
+            score = job_offer[job_id] - app_salary[app_id]
+            scores.append((job_id, score))
+
+        scores.sort(key=lambda x: x[1], reverse=True)
+        sorted_jobs = [x[0] for x in scores]
+        applicant_prefs[app_id] = sorted_jobs
+
+    return applicant_prefs, employer_prefs, employer_ranks
+
+def run_gale_shapley(applicants_df, jobs_df):
+    """Executes the O(n^2) Stable Matching algorithm."""
+    applicant_prefs, employer_prefs, employer_ranks = generate_preference_dicts(applicants_df, jobs_df)
+
     free_applicants = list(applicants_df['Applicant_ID'])
-    engagements = {} 
+    engagements = {}
     proposal_index = {app: 0 for app in free_applicants}
-    
+
     while free_applicants:
         app = free_applicants.pop(0)
         job = applicant_prefs[app][proposal_index[app]]
         proposal_index[app] += 1
-        
+
         if job not in engagements:
             engagements[job] = app
         else:
             current_app = engagements[job]
-            job_pref_list = employer_prefs[job]
-            
-            if job_pref_list.index(app) < job_pref_list.index(current_app):
+
+            # The O(1) Fix: Compare dictionary ranks instead of running .index() [cite: 72]
+            if employer_ranks[job][app] < employer_ranks[job][current_app]:
                 engagements[job] = app
                 free_applicants.append(current_app)
             else:
                 free_applicants.append(app)
-                
-    return engagements
 
-# Execution starts here
-if __name__ == "__main__":
-    applicants_df = pd.read_csv('applicants_N500.csv')
-    jobs_df = pd.read_csv('jobs_N500.csv')
-    matches = gale_shapley(applicants_df, jobs_df)
-    print("Final Matches (Job_ID : Applicant_ID):")
-    print(matches)
+    return engagements
